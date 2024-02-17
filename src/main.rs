51d8110f3f708cd414b978fa0e9b84a00cab47b6
Sai K K <saikksub@gmail.com>
2024-02-17 15:13:41 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::fs;
+use std::io;
+use std::path::Path;
+
+/*
+fn visit_dirs(dir: &Path) -> io::Result<()> {
+    if dir.is_dir() {
+        for entry in fs::read_dir(dir)? {
+            let entry = entry?;
+            let path = entry.path();
+            if path.is_dir() {
+                println!("Dir: {}", path.display());
+                visit_dirs(&path)?;
+            } else {
+                println!("File: {}", path.display());
+            }
+        }
+    }
+    Ok(())
+}
+*/
+
 fn main() {
     println!("Hello, world!");
+    let root = Path::new("/Users/kiransaisubramanyamkalimisetty/Projects/rust-file-sorter");
+    
+    if root.is_dir() {
+        println!()
+    }
 }
